29172bc3d364af7664a93e2ae1bdefa305c1cba9
Javier Lima <javi.limashelby@gmail.com>
2025-01-25 13:17:19 -0500

    feat: Clone environment MVP

---
+++ main.py
@@ -0,0 +1,37 @@
+import shutil
+import sys
+import os
+from magic import Magic
+
+if __name__ == '__main__':
+    SOURCE_ENV = sys.argv[1]
+    DEST_ENV = sys.argv[2]
+    SOURCE_SCRIPT_DIR = SOURCE_ENV + '/bin/'
+    DEST_SCRIPT_DIR = DEST_ENV + '/bin/'
+
+    shutil.copytree(SOURCE_ENV, DEST_ENV)
+
+    mime = Magic(mime=True)
+    scripts = []
+    for file in os.listdir(SOURCE_SCRIPT_DIR):
+        if mime.from_file(SOURCE_SCRIPT_DIR + file) == 'text/plain':
+            scripts.append(file)
+
+    for script in scripts:
+        source_script = SOURCE_SCRIPT_DIR + script
+        dest_script = DEST_SCRIPT_DIR + script
+        os.remove(dest_script)
+
+        with open(source_script, 'r') as file:
+            lines = file.readlines()
+
+        processed_lines = []
+        for line in lines:
+            processed_lines.append(line.replace(SOURCE_ENV[:-5], DEST_ENV[:-5]))
+
+        with open(dest_script, 'w') as file:
+            file.writelines(processed_lines)
+
+        os.chmod(dest_script, 0o755)
+    
+    print('Done')
